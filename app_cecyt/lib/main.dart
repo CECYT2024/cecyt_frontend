85a6f0d9ddcc7dcf4808106d76f91e92eb73ea6c
FDAB2001 <federi.al77@hotmail.com>
2024-05-30 14:22:11 -0400

    cambio de orden

--- app_cecyt/lib/main.dart
+++ app_cecyt/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:app_cecyt/features/auth/presentation/pages/pages.dart';
 import 'package:app_cecyt/features/home/ui/pages/pages.dart';
 import 'package:flutter/material.dart';
-import 'package:app_cecyt/features/home/start_page.dart';
+import 'package:app_cecyt/features/home/ui/start/start_page.dart';
 
 void main() {
   runApp(const MyApp());
